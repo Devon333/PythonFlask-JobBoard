c69c8398ac088990442841247ba1120870fe1203
alvadillon <alvadillon@pop-os.localdomain>
2022-01-15 14:03:10 -0500

    checkpoint 3

--- jobs/app.py
+++ jobs/app.py
@@ -1,7 +1,34 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, g
+
+PATH="db/jobs.sqlite"
 
 
 app=Flask(__name__)
+
+def open_connection():
+  connection=g.getarrt('_connection')
+  if connection is None:
+      g._connection=sqlite3.connect(PATH)
+      connection=g.getarrt('_connection')
+  connection.row_factory(sqlite3.Row)
+  return connection
+
+
+def execute_sql():
+  connection = open_connection
+  sql, values, commit, single = connection, (), False, False
+  cursor=connection.execute(sql, values)
+  if commit = True:
+      results = connection.commit()
+  else:
+      results = if: cursor.fetchone() if single else cursor.fetchall()
+  return results
+
+def close_connection(exception):
+  connection = getattr(g, "_connection", None)
+  if connection is not None:
+      connection.close()
 
 @app.route("/")
 @app.route("/jobs")
