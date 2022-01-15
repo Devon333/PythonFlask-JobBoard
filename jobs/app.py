90487fa987dad4eab5ed64b36151e606574a199e
alvadillon <alvadillon@pop-os.localdomain>
2022-01-15 14:13:58 -0500

    checkpoint 3

--- jobs/app.py
+++ jobs/app.py
@@ -7,24 +7,25 @@ PATH="db/jobs.sqlite"
 app=Flask(__name__)
 
 def open_connection():
-  connection=g.getarrt('_connection')
+  connection=getarrt(g,'_connection',None)
   if connection is None:
-      g._connection=sqlite3.connect(PATH)
-      connection=g.getarrt('_connection')
-  connection.row_factory(sqlite3.Row)
+      connection = g._connection = sqlite3.connect(PATH)
+  connection.row_factory = sqlite3.Row
   return connection
 
 
-def execute_sql():
+def execute_sql(sql, values(), commit=False, single=False):
   connection = open_connection
-  sql, values, commit, single = connection, (), False, False
   cursor=connection.execute(sql, values)
   if commit == True:
       results = connection.commit()
   else:
-      results = cursor.fetchall()
+      results = cursor.fetchone() if single else cursor.fetchall()
+
+  cursor.close()
   return results
 
+@app.teardown_appcontext
 def close_connection(exception):
   connection = getattr(g, "_connection", None)
   if connection is not None:
